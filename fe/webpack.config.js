ba250b05519c359dc0ec5f887a3e2674fb8d377f
chandureddivari <chandu.reddivari1504@gmail.com>
2016-12-17 13:39:07 +0530

    fixed chunking 404 error

--- fe/webpack.config.js
+++ fe/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   context: __dirname,
   entry: './js/BrowserEntry.jsx',
   output: {
-    path: path.join(__dirname, '/public/bundle'),
+    path: path.join(__dirname, '/public'),
     filename: 'bundle.js',
     publicPath: '/public/'
   },
