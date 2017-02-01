edd58ee5a6579ef8562c072c1774bdc89925e7fd
chandureddivari <chandu.reddivari1504@gmail.com>
2017-02-01 21:45:45 +0530

    Pluged in redux devtools

--- js/components/quiz/store.js
+++ js/components/quiz/store.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk),
+  typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : (f) => f
+))
 
 export default store
