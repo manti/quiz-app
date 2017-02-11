63d51f219b41724db4cb6344b3ded24d8d678460
chandureddivari <chandu.reddivari1504@gmail.com>
2017-02-11 14:53:30 +0530

    added review button

---
+++ js/components/quiz/Review.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Modal, Button, Table } from 'react-bootstrap'
+import { connect } from 'react-redux'
+const { object, bool } = require('react').PropTypes
+
+const Review = React.createClass({
+  propTypes: {
+    tests: object.isRequired,
+    fetchingTests: bool
+  },
+  getInitialState () {
+    return {show: false}
+  },
+
+  showModal () {
+    this.setState({show: true})
+  },
+
+  hideModal () {
+    this.setState({show: false})
+  },
+  render() {
+    return (
+      <div>
+        <Button onClick={this.showModal}>Review</Button>
+        <Modal
+          show={this.state.show}
+          onHide={this.hideModal}
+          bsSize="large" 
+          aria-labelledby="contained-modal-title-lg"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Review</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Click on a question to go there</h4>
+            <Table striped bordered condensed hover>
+                <thead>
+                  <tr>
+                    <th>Question Number</th>
+                    <th>Status</th>
+                    <th>Marked</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Answered</td>
+                    <td>Otto</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Not Answered</td>
+                    <td>Thornton</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Incomplete</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+            </Table>
+          </Modal.Body>
+        </Modal>
+      </div>
+    )
+  }
+})
+
+const mapStateToProps = (state) => {
+  return {
+    tests: state.tests,
+    fetchingTests: state.fetchingTests
+  }
+}
+
+export default connect(mapStateToProps)(Review)
--- js/components/quiz/Sidebar.js
+++ js/components/quiz/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import SectionTimer from './SectionTimer'
 import CalculatorHelp from './CalculatorHelp'
+import Review from './Review'
 const { object } = React.PropTypes
 
 const Sidebar = React.createClass({
@@ -11,8 +12,9 @@ const Sidebar = React.createClass({
   render () {
     return(
       <div>
-        <SectionTimer arg={this.props.arg} />
+        {/*<SectionTimer arg={this.props.arg} />*/}
         <CalculatorHelp arg={this.props.arg} />
+        <Review />
       </div>
     )
   }
