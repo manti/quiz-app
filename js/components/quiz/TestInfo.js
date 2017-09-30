e97c7a229f40add6e3ee90668c370ded997c87d5
chandureddyvari <chandrasekhar@getvymo.com>
2017-09-30 15:13:17 +0530

    fixed info and regulations page

--- js/components/quiz/TestInfo.js
+++ js/components/quiz/TestInfo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, Button } from 'react-bootstrap'
+import { Col, Button, Row } from 'react-bootstrap'
 import {hashHistory} from 'react-router'
 
 class TestInfo extends React.Component {
@@ -13,28 +13,64 @@ class TestInfo extends React.Component {
   render () {
     return (
       <div>
-        <div style={{'display': 'flex', 'justifyContent': 'flex-end'}} >
-          <Col xs={2} md={1}>
-            <Button onClick={this.onContinueClick}>Continue</Button>
+        <Row style={{backgroundColor: '#a5c5ef', padding: '15px'}}>
+          <Col md={4}>
+            <div style={{fontSize: '16px', fontWeight: 'bold'}}>
+              GREOnline®
+            </div>
+            <div>
+              [[Logo]]
+            </div>
           </Col>
-        </div>
-        <h3><b>General Test Information</b></h3>
-        <br />
-        <h4><b>Timing and Break</b></h4>
-        <br />
-        <p>There will be a 10-minute break following section 3 of the test. At that time notify the administrator if you wish to leave the room. Between sections you may wish to leave the room. Between sections you may pause for 60 seconds if you choose.</p>
-        <p>If you wish to leave your seat at any other time during the test, please raise your hand - section timing will not stop for this type of break.</p>
-        <br />
-        <h4><b>Test Information</b></h4>
-        <br />
-        <p>If you have a concern about the wording of a test question, please note the section number and the question number on your scratch paper and continue the test. Report your concern to the administrator after you complete the test.</p>
-        <p>No credit will be given for any responses marked on scratch paper. Use the scratch paper to work out your answers. All scratch paper must be turned in to the administrator at the end of the testing session.</p>
-        <p>In each of the Verbal Reasoning and Quantitative Reasoning sections, you may skip any question and return to that question later. You should answer as many questions as possible during each section and manage your time this in mind. Use the <b>Review</b> button at any time during the section to see which questions you have answered and which questions you have skipped.</p>
-        <br />
-        <h4><b>Repeater Policy</b></h4>
-        <br />
-        <p>You may take the General Test (computer-based and/or paper-based) only once per 21-day period, and no more than 5 times within any 12-month period. This applies even if you canceled your scores on the test taken previously. If you test more than once per 21-day period, or more than five times in a 12-month period, your new scores will not be reported, your test fee will be forfeited, and you may be banned from future testing.</p>
-        <p>Click <b>Continue</b> to proceed</p>
+          <Col xs={2} md={1} className='button-bg-tools pull-right' onClick={this.onContinueClick}>
+            <div style={{textAlign: 'center', position: 'relative', top: '10px'}}>
+              <div>Continue</div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5} mdPush={4}>
+            <h3><b>General Test Information</b></h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5} mdPush={5}>
+            <br />
+            <h4><b>Timing and Break</b></h4>
+            <br />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p>There will be a 10-minute break following section 3 of the test. At that time notify the administrator if you wish to leave the room. Between sections you may wish to leave the room. Between sections you may pause for 60 seconds if you choose.</p>
+            <p>If you wish to leave your seat at any other time during the test, please raise your hand - section timing will not stop for this type of break.</p>
+            <br />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5} mdPush={5}>
+            <h4><b>Test Information</b></h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <br />
+            <p>If you have a concern about the wording of a test question, please note the section number and the question number on your scratch paper and continue the test. Report your concern to the administrator after you complete the test.</p>
+            <p>No credit will be given for any responses marked on scratch paper. Use the scratch paper to work out your answers. All scratch paper must be turned in to the administrator at the end of the testing session.</p>
+            <p>In each of the Verbal Reasoning and Quantitative Reasoning sections, you may skip any question and return to that question later. You should answer as many questions as possible during each section and manage your time this in mind. Use the <b>Review</b> button at any time during the section to see which questions you have answered and which questions you have skipped.</p>
+            <br />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5} mdPush={5}><h4><b>Repeater Policy</b></h4></Col>
+        </Row>
+        <Row>
+          <Col>
+            <br />
+            <p>You may take the General Test (computer-based and/or paper-based) only once per 21-day period, and no more than 5 times within any 12-month period. This applies even if you canceled your scores on the test taken previously. If you test more than once per 21-day period, or more than five times in a 12-month period, your new scores will not be reported, your test fee will be forfeited, and you may be banned from future testing.</p>
+            <p>Click <b>Continue</b> to proceed</p>
+          </Col>
+        </Row>
       </div>
     )
   }
--- js/components/quiz/TestRegulations.js
+++ js/components/quiz/TestRegulations.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Col, Button } from 'react-bootstrap'
+import { Col, Button, Row } from 'react-bootstrap'
 import {hashHistory} from 'react-router'
 
 class TestRegulations extends React.Component {
@@ -13,33 +13,51 @@ class TestRegulations extends React.Component {
   render () {
     return (
       <div>
-        <div style={{'display': 'flex', 'justifyContent': 'flex-end'}} >
-          <Col xs={2} md={1}>
-            <Button onClick={this.onContinueClick}>Continue</Button>
+        <Row style={{backgroundColor: '#a5c5ef', padding: '15px'}}>
+          <Col md={4}>
+            <div style={{fontSize: '16px', fontWeight: 'bold'}}>
+              GREOnline®
+            </div>
+            <div>
+              [[Logo]]
+            </div>
           </Col>
-        </div>
-        <div>
-          <h3><b>Test Center Regulations</b></h3>
-          <p>You should have nothing on your computer table except your identification, pen or pencil, and provided scratch paper. Watch alarms must be turned off. You may not eat, drink, or use tobacco during the test. The official time will be kept by the computer. The administrator is authorized to dismiss you from the test session if you fail to follow directions or for other actions such as, but not limited to, the following. ETS will be notified of the action taken.</p>
-          <p>-attempting to take the test for someone else or having someone else take the test for you</p>
-          <p>-failing to provide acceptable identification</p>
-          <p>-obtaining improper access to the test, a part of the test, or information abut the test</p>
-          <p>-using a telephone or using and/or having a cell phone, PFA, or BlackBerry ® device in your possession during the test session or during breaks</p>
-          <p>-using any aids in connection with the test, such as mechanical pencils , pens, pagers, beepers, personal calculations, watch calculators, books, pamphlets, notes, unauthorized scratch paper, rulers, highlighter pens, stereos or radios with headphones, telephones, cell phones, watch alarms (including those with flashing lights or alarm sounds), stop watches, dictionaries, translators, and any hand-held electronic or photographic devices</p>
-          <p>-creating a disturbance</p>
-          <p>-attempting to give or receive assistance: communication in any from is not permitted during the test administration, and discussion or sharing of test content or answers during and after the test administration is prohibited</p>
-          <p>-removing or attempting ro remove test content from the test center under no circumstances may test content or any part of the test content be removed, reproduced and/or disclosed by any means (e.g., hard copy, verbally, electronically) to any person or entity</p>
-          <p>-tampering with a computer</p>
-          <p>-attempting to remove scratch paper from the testing room or using scratch paper during untimed sections of the test or during breaks</p>
-          <p>-bringing a weapon or firearm into the test center</p>
-          <p>-bringing food, drinks, or tobacco into the testing room</p>
-          <p>-leaving the test center vicinity during the test session or during breaks</p>
-          <p>-leaving the testing room without permission</p>
-          <p>-taking excessive or extended unscheduled breaks during the test session: test center administrators are required to strictly monitor unscheduled breaks and report test takers who take excessive or extended breaks</p>
-          <p>-failing to follow any of the test administrator, or specified in any test materials</p>
-          <p>ETS reserves the right to take all action – including, but not limited to, barring you from future testing and/or canceling your scores – for failure to comply with test administration regulations or the test administrator’s directions. If your scores are canceled, they will not be reported, and your fees will not be refunded.</p>
-          <p>Click <b>Continue</b> to proceed</p>
-        </div>
+          <Col xs={2} md={1} className='button-bg-tools pull-right' onClick={this.onContinueClick}>
+            <div style={{textAlign: 'center', position: 'relative', top: '10px'}}>
+              <div>Continue</div>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5} mdPush={4}>
+            <h3><b>Test Center Regulations</b></h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div>
+              <p>You should have nothing on your computer table except your identification, pen or pencil, and provided scratch paper. Watch alarms must be turned off. You may not eat, drink, or use tobacco during the test. The official time will be kept by the computer. The administrator is authorized to dismiss you from the test session if you fail to follow directions or for other actions such as, but not limited to, the following. ETS will be notified of the action taken.</p>
+              <p>-attempting to take the test for someone else or having someone else take the test for you</p>
+              <p>-failing to provide acceptable identification</p>
+              <p>-obtaining improper access to the test, a part of the test, or information abut the test</p>
+              <p>-using a telephone or using and/or having a cell phone, PFA, or BlackBerry ® device in your possession during the test session or during breaks</p>
+              <p>-using any aids in connection with the test, such as mechanical pencils , pens, pagers, beepers, personal calculations, watch calculators, books, pamphlets, notes, unauthorized scratch paper, rulers, highlighter pens, stereos or radios with headphones, telephones, cell phones, watch alarms (including those with flashing lights or alarm sounds), stop watches, dictionaries, translators, and any hand-held electronic or photographic devices</p>
+              <p>-creating a disturbance</p>
+              <p>-attempting to give or receive assistance: communication in any from is not permitted during the test administration, and discussion or sharing of test content or answers during and after the test administration is prohibited</p>
+              <p>-removing or attempting ro remove test content from the test center under no circumstances may test content or any part of the test content be removed, reproduced and/or disclosed by any means (e.g., hard copy, verbally, electronically) to any person or entity</p>
+              <p>-tampering with a computer</p>
+              <p>-attempting to remove scratch paper from the testing room or using scratch paper during untimed sections of the test or during breaks</p>
+              <p>-bringing a weapon or firearm into the test center</p>
+              <p>-bringing food, drinks, or tobacco into the testing room</p>
+              <p>-leaving the test center vicinity during the test session or during breaks</p>
+              <p>-leaving the testing room without permission</p>
+              <p>-taking excessive or extended unscheduled breaks during the test session: test center administrators are required to strictly monitor unscheduled breaks and report test takers who take excessive or extended breaks</p>
+              <p>-failing to follow any of the test administrator, or specified in any test materials</p>
+              <p>ETS reserves the right to take all action – including, but not limited to, barring you from future testing and/or canceling your scores – for failure to comply with test administration regulations or the test administrator’s directions. If your scores are canceled, they will not be reported, and your fees will not be refunded.</p>
+              <p>Click <b>Continue</b> to proceed</p>
+            </div>
+          </Col>
+        </Row>
       </div>
     )
   }
